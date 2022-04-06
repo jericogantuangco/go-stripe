9d192275f2fb7b07dec2494bddfb49978601a129
Jerico <jericogantuangco200@gmail.com>
2022-04-06 12:56:17 +0800

    initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Get("/virtual-terminal", app.VirtualTerminal)
+
+	return mux
+}
